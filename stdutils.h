cc04ecf937e74577244a07fef51391200de0bab8
guitarpicva <mitch@radcommsoft.com>
2022-06-16 16:48:40 -0400

    add comments

--- stdutils.h
+++ stdutils.h
@@ -1,7 +1,14 @@
 #include <string>
 #include <vector>
 
-static void split(std::string str, std::vector<string> &token_v, const char delim){
+// We all need to split a string on a delimiter!
+// This could be extended to split on another std::string
+// or to optionally drop the empty tokens.  Lots of choices in std::string
+
+// This version splits on a single char and keeps all empty tokens, so:
+// split on "a,b,c,,,f,g,h" with 'c' as the delimiter returns a vector 
+// with size of 8 with two empty strings in indexes 3 and 4.
+static void split(std::string str, std::vector<std::string> &token_v, const char delim){
     size_t start = str.find_first_not_of(delim), end=start;
     while (start != std::string::npos){
         // Find next occurence of delimiter
@@ -9,7 +16,7 @@ static void split(std::string str, std::vector<string> &token_v, const char deli
         // Push back the token found into vector
         // back to back tokens leave an empty string in the vector
         token_v.push_back(str.substr(start, end-start));
-        // Skip all occurences of the delimiter to find new start
-        start = end + 1
+        // slide up one to get to the next starting point
+        start = end + 1;
     }
 }
