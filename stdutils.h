3afe674facee48c3f9debbbbe22d536f2b28cc99
guitarpicva <mitch@radcommsoft.com>
2022-06-17 09:53:34 -0400

    add bool check to third function

--- stdutils.h
+++ stdutils.h
@@ -36,3 +36,25 @@ static void split_discard(std::string str, std::vector<std::string> &token_v, co
         start = str.find_first_not_of(delim, end);
     }
 }
+
+// This version splits on a single char and keeps/discards all empty tokens
+// based on bool passed. true = keep, false = discard
+// with size of 6 containing <a b c f g h>
+static void split(std::string str, std::vector<std::string> &token_v, const char delim, bool keep_empty_tokens = true){
+    size_t start = str.find_first_not_of(delim), end=start;
+    while (start != std::string::npos){
+        // Find next occurence of delimiter
+        end = str.find(delim, start);
+        // Push back the token found into vector
+        // back to back tokens leave an empty string in the vector
+        token_v.push_back(str.substr(start, end-start));
+        // choose to keep or discard
+        if(keep_empty_tokens) {
+            start = end + 1;
+        }
+        else {}
+            // slide up to the next non-delimiter char (skips empty fields)
+           start = str.find_first_not_of(delim, end);
+        }
+    }
+}
